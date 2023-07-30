d6dd17e3d7b18f5dc3e181d2ecb818e78ccc2a6c
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-07-30 20:23:57 -0300

    Almost finish the functionalities. I found some erros that I must to solve soon

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,6 +5,7 @@ function createCalculator() {
 
     run() {
       this.clickButtons();
+      this.enterPressed();
     },
 
     clickButtons() {
@@ -15,8 +16,34 @@ function createCalculator() {
 
         if(element.classList.contains('btn-clear')) this.eraseButton();
 
-        if(element.classList.contains('bnt-del')) this.ereaseOne();
+        if(element.classList.contains('btn-del')) this.eraseOne();
 
+        if(element.classList.contains('btn-equal')) this.makeCount();
+      })
+    },
+
+    makeCount() {
+      let count = this.display.value;
+      
+      try {
+        count = eval(count);
+
+        if(!count) {
+          alert('Invalid Value!');
+          return;
+        }
+
+        this.display.value = count;
+
+      } catch(e) {
+        alert('Invalide Value!');
+        return; 
+      }
+    },
+
+    enterPressed() {
+      this.addEventListener("keyup", (e) => {
+        if(e.keyCode === 13) this.makeCount();
       })
     },
 
